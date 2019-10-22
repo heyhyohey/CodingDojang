dc4b7b789c0de0eb172596e1472c5ef268db41bb
heyhyohey <hyoje420@gmail.com>
2019-10-22 18:08:19 +0900

    2019-10-22 heyhyohey add a linked list problem

---
+++ linked_list/linked_list.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct element {
+	int numerator, denominator;
+} element;
+
+typedef struct rat_node {   // 단순 연결리스트 노드 구조
+    element ratio;      // 모두 양수 또는 음수일 수 있음
+    struct rat_node *link;
+} rat_node;
+
+typedef struct rational {   // 다항식 표현의 구조체
+    int length;
+    rat_node *head;
+} rational;
+
+rational* init_list() {
+	rational* r = (rational*)malloc(sizeof(rational));
+	r->length = 0;
+	r->head = NULL;
+
+	return r;
+}
+
+int main(int argc, char* argv[]) {
+	rational *r;
+
+	// 1. 리스트 초기화
+	r = init_list();
+	
+	printf("%d %x\n", r->length, r->head);
+
+	return 0;
+}
